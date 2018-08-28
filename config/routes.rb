cdb97d7657e210fd9157df7a358654da288c9a10
Reece_Collett <rcollett@spartaglobal.com>
2018-08-28 15:23:46 +0100

    Added 3 resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  resources :customers
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get "/", to: "posts#index"
+
+  get "/statics", to: "statics#home"
+
+  root "customers#index"
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_08_28_133445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.string "firstname"
+    t.string "lastname"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
